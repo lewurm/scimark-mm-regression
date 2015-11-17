d5dc44a36b32c19e3ba5700e9f3bfbb55428ff0e
Bernhard Urban <bernhard.urban@xamarin.com>
2015-11-16 16:31:14 -0800

    Initial commit

---
+++ hax.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+public class Hax {
+	public static void Main() {
+		var info = new ProcessStartInfo {
+			UseShellExecute = false,
+			RedirectStandardOutput = true,
+			RedirectStandardError = true,
+		};
+		info.EnvironmentVariables.Clear ();
+		info.EnvironmentVariables["LANG"] = "C.UTF-8";
+		info.FileName = "/home/lewurm/monoperf/installation/opt/mono-2015.10.29+01.40.50/bin/mono-sgen";
+		info.WorkingDirectory = "/home/lewurm/monoperf/benchmarker/tests/scimark/";
+		info.Arguments = "--stats scimark.exe MM";
+		using (Process exe = Process.Start (info)) {
+			exe.WaitForExit ();
+			var stdout = Task.Factory.StartNew (() => new StreamReader (exe.StandardOutput.BaseStream).ReadToEnd (), TaskCreationOptions.LongRunning);
+			var stderr = Task.Factory.StartNew (() => new StreamReader (exe.StandardError.BaseStream).ReadToEnd (), TaskCreationOptions.LongRunning);
+
+			if (exe.ExitCode != 0) {
+				Console.Out.WriteLine ("forking failed a bit");
+			}
+
+			Console.Out.WriteLine ("stdout:\n{0}", stdout.Result);
+			Console.Out.WriteLine ("stderr:\n{0}", stderr.Result);
+		}
+	}
+}
+
