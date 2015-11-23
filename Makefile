a4431b9a463f6b1034a77accfd49c62ec921be94
Bernhard Urban <bernhard.urban@xamarin.com>
2015-11-23 19:23:02 +0000

    perf

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SHELL := bash
 
-all: slow.exe fast.exe
+all: slow.exe fast.exe woot
 
 slow.exe: hax.cs
 	mcs -d:SLOW $<
@@ -9,3 +9,7 @@ slow.exe: hax.cs
 fast.exe: hax.cs
 	mcs -d:FAST $<
 	mv hax.exe $@
+
+woot woot.s: woot.c Makefile
+	gcc -Wall -Werror -O1 $< -o woot
+	gcc -Wall -Werror -O1 -S $<
--- hax.cs
+++ hax.cs
@@ -15,10 +15,17 @@ public class Hax {
 		exe.StartInfo.EnvironmentVariables["LANG"] = "C.UTF-8";
 #endif
 		// exe.StartInfo.FileName = "/Users/bernhardu/work/mono/b/bin/mono";
-		exe.StartInfo.FileName = "/home/lewurm/monoperf/installation/opt/mono-2015.10.29+01.40.50/bin/mono-sgen";
+		// exe.StartInfo.FileName = "/home/lewurm/monoperf/installation/opt/mono-2015.10.29+01.40.50/bin/mono-sgen";
 		// exe.StartInfo.WorkingDirectory = "/Users/bernhardu/work/benchmarker/tests/scimark/";
 		exe.StartInfo.WorkingDirectory = "/home/lewurm/monoperf/benchmarker/tests/scimark/";
-		exe.StartInfo.Arguments = "--stats -O=-aot scimark.exe MM";
+
+		exe.StartInfo.FileName = "/home/lewurm/monoperf/linux/tools/perf/perf";
+		exe.StartInfo.Arguments =  "stat -d  /home/lewurm/monoperf/monobuild/bin/mono-sgen -O=-aot scimark.exe MM";
+#if false
+		// gdb fun
+		exe.StartInfo.FileName = "gdb";
+		exe.StartInfo.Arguments =  "--args /home/lewurm/monoperf/monobuild/bin/mono-sgen --stats --break SparseCompRow:matmult -O=-aot scimark.exe MM";
+#endif
 
 		exe.OutputDataReceived += (sender, args) => Console.WriteLine ("{0}", args.Data);
 		exe.ErrorDataReceived += (sender, args) => Console.WriteLine ("{0}", args.Data);
